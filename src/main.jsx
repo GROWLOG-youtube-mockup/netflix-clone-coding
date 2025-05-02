e7d7ba3607fc42b58aa9a1956b0e1c3c5da37cfb
Hyuna Kang <kho8815@gmail.com>
2025-05-02 17:37:24 +0900

    fix: 오타 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import './styles/index.css';
 
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowerRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowerRouter>
+    <BrowserRouter>
       <App />
-    </BrowerRouter>
+    </BrowserRouter>
   </StrictMode>
 );
