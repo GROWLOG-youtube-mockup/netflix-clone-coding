338d20f5e26886dbef9bc44a24673c7a690b891b
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-02 17:17:24 +0900

    refactor: 환경변수 설정 Vite 형식으로 변경 및 개인 인증 토큰 추가
    - headers에 인증 추가
    - VITE가 인식할 수 있도록 import.meta 사용

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const movieApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
+
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${import.meta.env.VITE_MOVIE_DB_TOKEN_KEY}`
+  },
   params: {
-    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+    api_key: import.meta.env.VITE_MOVIE_DB_API_KEY,
     language: 'ko-KR'
   }
 });
