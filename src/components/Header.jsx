a1851aabf73b54bb99666ad8c2b3c48e93e47eec
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-04 06:23:14 +0900

    feat: Header UI 개발
    - 이미지 카테코리를 제외한 이미지 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
+import '../styles/Header.css';
+
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
+import notification from '../assets/notification.svg';
+import profile from '../assets/profile.png';
+import searchIcon from '../assets/search.svg';
 
 function Header() {
   return (
     <div>
+      <div className="header">
+        <div className="header-left">
+          <Link to="/">
+            <img className="header-left-logo" alt="Netflix logo" src={logo} />
+          </Link>
+        </div>
+        <div className="header-category" />
+        <div className="header-right">
+          <img className="header-right-search-icon" alt="Netflix search icon" src={searchIcon} />
+          <img
+            className="header-right-notification"
+            alt="Netflix Notification"
+            src={notification}
+          />
+          <img className="header-right-profile" alt="Netflix profile" src={profile} />
+        </div>
+      </div>
       <h2>Header</h2>
     </div>
   );
