efb00c228b3966d9578e53141711199a33496824
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-06 07:19:33 +0900

    feat: Hero 섹션 구현을 위한 라우팅 설정

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 
 import Footer from '../components/Footer.jsx';
 import Header from '../components/Header.jsx';
+import Hero from '../components/Hero.jsx';
 import CategoryPage from '../pages/CategoryPage/index.jsx';
 import MainPage from '../pages/MainPage/index.jsx';
 
@@ -9,6 +10,7 @@ function Layout() {
   return (
     <div>
       <Header />
+      <Hero />
       <Outlet />
       <Footer />
     </div>
