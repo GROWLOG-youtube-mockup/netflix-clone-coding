57c27c985af410bccd74bdcd24fd50feb3ffcda1
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-07 09:23:13 +0900

    feat: 영화 선택 및 모달 사용 여부, 클릭 이벤트 추가

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import requests from '../../api/requests.js';
 import CardsGridView from '../../components/CardsGridView.jsx';
+import PreviewModal from './PreviewModal/PreviewModal.jsx';
 
 function MainPage() {
+  const [selectedMovie, setSelectedMovie] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const clickHandle = (movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
   return (
     <div>
       <CardsGridView fetchUrl={requests.trending} />
