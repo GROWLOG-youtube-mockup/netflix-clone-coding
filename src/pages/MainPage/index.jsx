cc35a5e9baa3458588d0ad4a67bf281110468c41
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-07 09:27:17 +0900

    fix: modal 사용 조건 추가 및 import 수정

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import requests from '../../api/requests.js';
 import CardsGridView from '../../components/CardsGridView.jsx';
-import PreviewModal from './PreviewModal/PreviewModal.jsx';
+import PreviewModal from '../../components/PreviewModal/PreviewModal.jsx';
 
 function MainPage() {
   const [selectedMovie, setSelectedMovie] = useState();
@@ -15,7 +15,11 @@ function MainPage() {
 
   return (
     <div>
-      <CardsGridView fetchUrl={requests.trending} />
+      <CardsGridView fetchUrl={requests.trending} clickHandle={clickHandle} />
+
+      {isModalOpen && selectedMovie && (
+        <PreviewModal selectedMovie={selectedMovie} setModalOpen={isModalOpen} />
+      )}
     </div>
   );
 }
