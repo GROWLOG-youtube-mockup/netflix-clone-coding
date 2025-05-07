a0cb8861d9a7f898dd149329ff9e32b51b492c97
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-07 09:44:50 +0900

    fix: 클릭 이벤트로 인해 동작될 함수를 호출하지 않고 참조할 수 있도록 수정함

--- src/components/PreviewModal/PreviewModal.jsx
+++ src/components/PreviewModal/PreviewModal.jsx
@@ -3,13 +3,17 @@ import '../../styles/PreviewModal.css';
 import React from 'react';
 
 function PreviewModal({ selectedMovie, setModalOpen }) {
-  console.log(selectedMovie, setModalOpen);
+  console.log(selectedMovie);
+  console.log(setModalOpen);
 
   return (
-    <div className="preview-layout">
+    <div className="preview-modal">
       <div className="preview-title-wrapper">
         <div className="preview-title-background">
-          <img src="" alt="" />
+          <img
+            src={`https://image.tmdb.org/t/p/original/${selectedMovie?.backdrop_path}`}
+            alt="modal__poster-img"
+          />
         </div>
         <div className="button-layout">
           <button type="button">PLAY</button>
