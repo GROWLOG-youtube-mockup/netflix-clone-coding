f7d80e612f8d81b20aa3523cc191a13857d5c569
Hyuna Kang <kho8815@gmail.com>
2025-05-07 22:15:02 +0900

    style: 검색 결과 페이지 css 추가
    (cherry picked from commit b2ef4b2fa90a4195540a1ffb79d6f5e5fd70af15)

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import CardsGridView from '../../components/CardsGridView.jsx';
+import '../../styles/SearchPage.css';
 
 function SearchPage() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('keyword');
 
   return (
-    <div>
+    <div className="search-results-container">
       <CardsGridView
         fetchUrl={`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`}
       />
