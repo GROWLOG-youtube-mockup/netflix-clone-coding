083bc68da346dd1e42a8e2c8f76f5b179b017e37
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-07 10:49:35 +0900

    fix: modal layout

--- src/components/PreviewModal/PreviewModal.jsx
+++ src/components/PreviewModal/PreviewModal.jsx
@@ -7,27 +7,31 @@ function PreviewModal({ selectedMovie, setModalOpen }) {
   console.log(setModalOpen);
 
   return (
-    <div className="preview-modal">
-      <div className="preview-title-wrapper">
-        <div className="preview-title-background">
-          <img
-            src={`https://image.tmdb.org/t/p/original/${selectedMovie?.backdrop_path}`}
-            alt="modal__poster-img"
-          />
+    <div className="presentation">
+      <div className="preview-modal">
+        <div className="preview-title-wrapper">
+          <div className="preview-title-background">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${selectedMovie?.backdrop_path}`}
+              alt="modal__poster-img"
+            />
+          </div>
+          <div className="button-layout">
+            <button type="button" className="play-button">
+              PLAY
+            </button>
+            <button type="button">HAS</button>
+            <button type="button">LIKE</button>
+          </div>
         </div>
-        <div className="button-layout">
-          <button type="button">PLAY</button>
-          <button type="button">HAS</button>
-          <button type="button">LIKE</button>
-        </div>
-      </div>
 
-      <div className="preview-detail-container">
-        <div className="meta-data">
-          <span>detail</span>
-        </div>
-        <div>
-          <span>detail</span>
+        <div className="preview-detail-container">
+          <div className="meta-data">
+            <span>detail</span>
+          </div>
+          <div>
+            <span>detail</span>
+          </div>
         </div>
       </div>
     </div>
