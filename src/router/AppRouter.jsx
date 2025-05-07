7009888f9f4515c073e2f17cdde2741c10a21a75
Hyuna Kang <kho8815@gmail.com>
2025-05-07 22:08:18 +0900

    feat: 검색 기능 및 화면 구현
    - AppRouter.jsx 내 search 경로 추가
    - 헤더 검색창 통해 검색어 입력
    - 검색어 입력 시 검색 페이지로 이동
    - 검색어는 쿼리 파라미터 키워드로 포함
    - 검색 페이지에서 CardGridView 컴포넌트 호출

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer.jsx';
 import Header from '../components/Header.jsx';
 import CategoryPage from '../pages/CategoryPage/index.jsx';
 import MainPage from '../pages/MainPage/index.jsx';
+import SearchPage from '../pages/SearchPage/index.jsx';
 
 function Layout() {
   return (
@@ -22,6 +23,7 @@ export default function AppRouter() {
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path="genre/:genreId" element={<CategoryPage />} />
+          <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
     </div>
