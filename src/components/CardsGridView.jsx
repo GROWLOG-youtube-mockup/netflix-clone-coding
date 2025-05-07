7d463770236ee2fefddd2c7b6c77596e6d74ecd8
Hyuna Kang <kho8815@gmail.com>
2025-05-07 20:29:00 +0900

    feat: 검색 중이거나 검색 결과 없는 케이스 추가
    (cherry picked from commit ff03246fb46773a4aeab433dfca1d969a16dde10)

--- src/components/CardsGridView.jsx
+++ src/components/CardsGridView.jsx
@@ -6,16 +6,40 @@ import api from '../api/api.js';
 
 function CardsGridView({ fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   async function fetchMovieData() {
-    const request = await api.get(fetchUrl);
-    setMovies(request.data.results);
-    return request;
+    try {
+      const request = await api.get(fetchUrl);
+      setMovies(request.data.results);
+      setIsLoaded(true);
+    } catch (error) {
+      console.error('검색 실패', error);
+    } finally {
+      setIsLoaded(true);
+    }
   }
   useEffect(() => {
     fetchMovieData();
-  });
+  }, [fetchUrl]);
 
+  if (!isLoaded) {
+    return <div className="search-loding-msg">검색 중입니다</div>;
+  }
+  if (movies.length === 0) {
+    return (
+      <div className="search-novalue-msg">
+        <div>입력하신 검색어와 일치하는 결과가 없습니다.</div>
+        <br />
+        <ul>
+          <li>다른 키워드를 입력해 보세요.</li>
+          <li>시리즈나 영화를 찾고 있으신가요?</li>
+          <li>영화 제목, 시리즈 제목, 또는 배우나 감독의 이름으로 검색해 보세요.</li>
+          <li>코미디, 로맨스, 스포츠 또는 드라마와 같은 장르명으로 검색해 보세요.</li>
+        </ul>
+      </div>
+    );
+  }
   return (
     <section className="cards-grid">
       <div className="grid-container">
