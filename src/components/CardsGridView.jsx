10e9a7ed6136c4c138e37d298e621461e1396d8f
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-07 09:26:33 +0900

    fix: 클릭 이벤트 전달

--- src/components/CardsGridView.jsx
+++ src/components/CardsGridView.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 import api from '../api/api.js';
 
-function CardsGridView({ fetchUrl }) {
+function CardsGridView({ fetchUrl, clickHandle }) {
   const [movies, setMovies] = useState([]);
 
   async function fetchMovieData() {
@@ -12,6 +12,7 @@ function CardsGridView({ fetchUrl }) {
     setMovies(request.data.results);
     return request;
   }
+
   useEffect(() => {
     fetchMovieData();
   });
@@ -20,7 +21,14 @@ function CardsGridView({ fetchUrl }) {
     <section className="cards-grid">
       <div className="grid-container">
         {movies.map((movie, index) => (
-          <div key={movie.id} className={`grid-poster poster-${index}`}>
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <div
+            role="button"
+            tabIndex={index}
+            key={movie.id}
+            className={`grid-poster poster-${index}`}
+            onClick={clickHandle}
+          >
             <img
               src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
               alt={movie.title}
