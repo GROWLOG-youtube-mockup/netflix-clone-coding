1b8e8d1f0e5a8aff217cdec9ab859350c3aca5c7
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-07 15:24:55 +0900

    feat: 각 페이지의 노출되어야하는 섹션 표시
    - mainpage: 히어로 베너
    - categorypage: sub header, 히어로 베너

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Hero from '../../components/Hero.jsx';
+
 function MainPage() {
   return (
     <div>
+      <Hero />
       <h2>MainPage</h2>
     </div>
   );
