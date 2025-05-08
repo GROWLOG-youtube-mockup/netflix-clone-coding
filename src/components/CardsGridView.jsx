920ef62e93585e38dbae74f88dbe5bc032d4e04c
Hyuna Kang <kho8815@gmail.com>
2025-05-08 19:52:40 +0900

    fix: develop 병합 충돌 해결

--- src/components/CardsGridView.jsx
+++ src/components/CardsGridView.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 import api from '../api/api.js';
 
-function CardsGridView({ fetchUrl }) {
+function CardsGridView({ fetchUrl, clickHandle }) {
   const [movies, setMovies] = useState([]);
 
   async function fetchMovieData() {
@@ -12,6 +12,7 @@ function CardsGridView({ fetchUrl }) {
     setMovies(request.data.results);
     return request;
   }
+
   useEffect(() => {
     fetchMovieData();
   });
@@ -19,8 +20,16 @@ function CardsGridView({ fetchUrl }) {
   return (
     <section className="cards-grid">
       <div className="grid-container">
-        {movies.map((movie, index) => (
-          <div key={movie.id} className={`grid-poster poster-${index}`}>
+        {/* 영화 검색되는 경우 */}
+        {movies?.map((movie, index) => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <div
+            role="button"
+            tabIndex={index}
+            key={movie.id}
+            className={`grid-poster poster-${index}`}
+            onClick={() => clickHandle(movie)}
+          >
             <img
               src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
               alt={movie.title}
