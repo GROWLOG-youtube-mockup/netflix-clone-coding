5ef094060c825e1aeb42c72ef49e088d56ff0cc9
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-09 04:34:50 +0900

    feat: MainPage에 표시되는 카테고리 별 영상들 와이드 프레임으로 제작
    - 반응형 구현
    - BannersSlider.jsx와 BannersSlider.css 파일 생성
    - CardsSlider.jsx와 CardsSlider.css를 참고하여 제작

---
+++ src/components/BannersSlider.jsx
@@ -0,0 +1,64 @@
+import '../styles/BannersSlider.css';
+import React, { useEffect, useRef, useState } from 'react';
+
+import api from '../api/api.js';
+
+function BannersSlider({ title, fetchUrl }) {
+  const [contents, setContents] = useState([]);
+  const sliderRef = useRef(null);
+
+  async function fetchContentData() {
+    const request = await api.get(fetchUrl);
+    setContents(request.data.results.filter((content) => content.backdrop_path));
+    return request;
+  }
+
+  useEffect(() => {
+    fetchContentData();
+  }, [fetchUrl]);
+
+  function handleClickArrow(dir) {
+    const slider = sliderRef.current;
+    slider.scrollLeft =
+      dir === 'left'
+        ? slider.scrollLeft - (window.innerWidth - 80)
+        : slider.scrollLeft + (window.innerWidth - 80);
+  }
+
+  return (
+    <section className="banners-row">
+      <h2 className="banners-row-title">{title}</h2>
+      <div className="banner-slider-container">
+        <button
+          className="banner-slider-arrow arrow-left"
+          onClick={() => handleClickArrow('left')}
+          type="button"
+        >
+          {'<'}
+        </button>
+        <div className="banner-slider-content" ref={sliderRef}>
+          {contents.map((content, index) => (
+            <div key={content.id} className={`banner-item banner-${index}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/original${content.backdrop_path}`}
+                alt={content.title || content.name}
+              />
+              <div className="banner-info">
+                <h3>{content.title || content.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="banner-slider-arrow arrow-right"
+          onClick={() => handleClickArrow('right')}
+          type="button"
+        >
+          {'>'}
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default BannersSlider;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import requests from '../../api/requests.js';
-import CardsGridView from '../../components/CardsGridView.jsx';
+import BannersSlider from '../../components/BannersSlider.jsx';
 import Hero from '../../components/Hero.jsx';
 
 function MainPage() {
   return (
     <div>
       <Hero />
-      <CardsGridView fetchUrl={requests.trendingMovies} />
+      <BannersSlider title="TOP 10 시리즈" fetchUrl={requests.top10Series} />
+      <BannersSlider title="지금 뜨는 콘텐츠" fetchUrl={requests.trendingAll} />
+      <BannersSlider title="넷플릭스 오리지널" fetchUrl={requests.netflixOriginals} />
+      <BannersSlider title="한국 드라마" fetchUrl={requests.koreanDramaSeries} />
+      <BannersSlider title="한국 영화" fetchUrl={requests.koreanMovies} />
+      <BannersSlider title="애니메이션" fetchUrl={requests.animationSeries} />
+      <BannersSlider title="리얼리티 TV" fetchUrl={requests.realityTV} />
+      <BannersSlider title="SF & 판타지" fetchUrl={requests.sciFiSeries} />
+      <BannersSlider title="드라마" fetchUrl={requests.dramaSeries} />
+      <BannersSlider title="범죄 영화" fetchUrl={requests.crimeMovies} />
+      <BannersSlider title="가족 영화" fetchUrl={requests.familyContent} />
     </div>
   );
 }
