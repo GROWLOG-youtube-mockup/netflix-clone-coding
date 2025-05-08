1ef1135f535022e50492a82605cf6959c121a59d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-08 16:46:58 +0900

    style: Footer social icon 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import '../styles/Footer.css';
+
+import facebook from '../assets/facebook.svg';
+import instagram from '../assets/instagram.svg';
+import twitter from '../assets/twitter.svg';
+import youtube from '../assets/youtube.svg';
+
 function Footer() {
+  const socialLinks = [
+    { name: 'facebook', icon: facebook },
+    { name: 'instagram', icon: instagram },
+    { name: 'twitter', icon: twitter },
+    { name: 'youtube', icon: youtube }
+  ];
+
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="footer">
+      <div className="social-links">
+        {socialLinks.map(({ name, icon }) => (
+          <div key={name} className="social-link">
+            <img src={icon} alt="name" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
