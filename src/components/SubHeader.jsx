eee6d6975296c91cdf8c9d57e2fe6fdbd8368d1c
Hyuna Kang <kho8815@gmail.com>
2025-05-08 19:04:43 +0900

    feat: 카테고리 페이지 별 row/grid 뷰 구현
    - 카테고리에 따라 콘텐츠 타입(시리즈/영화) 분류
    - row/grid 방식에 따라 화면 구현

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,19 +1,17 @@
 import '../styles/Header.css';
 
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import { categoryConfig } from '../api/categoryConfig.js';
 import gridToggle from '../assets/grid-toggle.svg';
 import rowToggle from '../assets/row-toggle.svg';
 import triangleDown from '../assets/triangledown_106509.svg';
 
-function SubHeader({ setView }) {
+function SubHeader({ setView, selectedCategory }) {
   const navigate = useNavigate();
-  const { genreId } = useParams();
   const { search } = useLocation();
   const searchParams = new URLSearchParams(search);
-  const selectedCategory = genreId || 'home';
   const subKey = searchParams.get('sub');
 
   const [show, setShow] = useState(false);
--- src/pages/CategoryPage/index.jsx
+++ src/pages/CategoryPage/index.jsx
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { categoryConfig } from '../../api/categoryConfig.js';
 import requests from '../../api/requests.js';
 import CardsGridView from '../../components/CardsGridView.jsx';
 import CardsSlider from '../../components/CardsSlider.jsx';
 import Hero from '../../components/Hero.jsx';
 import SubHeader from '../../components/SubHeader.jsx';
+
 import '../../styles/CategoryPage.css';
 
 function CategoryPage() {
   const [view, setView] = useState('row');
+  const { genreId } = useParams();
+
+  const selectedCategory = genreId || 'home';
+  const categoryRequest = selectedCategory === 'series' ? categoryConfig[1] : categoryConfig[2];
 
   return (
     <div>
-      <SubHeader setView={setView} />
+      <SubHeader setView={setView} selectedCategory={selectedCategory} />
       {view === 'row' && (
         <div>
           <Hero />
-          <CardsSlider title="코미디 시리즈" fetchUrl={requests.comedySeries} />
+          {categoryRequest.subGenres.map((sub) => (
+            <CardsSlider title={sub.title} fetchUrl={requests[sub.requestKey]} key={sub.key} />
+          ))}
         </div>
       )}
-      {/* url 임시로 설정 */}
       {view === 'grid' && (
         <div className="cards-grid-view-container">
-          <CardsGridView fetchUrl={requests.netflixOriginals} />
+          <CardsGridView fetchUrl={requests[categoryRequest.requestKey]} />
         </div>
       )}
     </div>
