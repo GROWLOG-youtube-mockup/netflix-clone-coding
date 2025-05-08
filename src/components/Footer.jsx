b2d1758be0e15172b33967e550ee7f0322a6b140
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-08 17:19:12 +0900

    feat: footer member 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,12 +15,22 @@ function Footer() {
     { name: 'youtube', icon: youtube }
   ];
 
+  const footerMembers = ['강현아', '김유진', '김지은', '박건', '박재경', '이승준', '이혜원'];
+
   return (
     <div className="footer">
       <div className="social-links">
         {socialLinks.map(({ name, icon }) => (
           <div key={name} className="social-link">
             <img src={icon} alt="name" />
+          </div>
+        ))}
+      </div>
+
+      <div className="footer-members">
+        {footerMembers.map((name) => (
+          <div key={name} className="footer-member">
+            <span>{name}</span>
           </div>
         ))}
       </div>
