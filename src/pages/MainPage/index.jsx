90d51b6a590b3083194ef89a4abaa8dff385b3ce
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-09 19:12:32 +0900

    refactor: MainPage 컴포넌트 구조 개선

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -5,20 +5,26 @@ import BannersSlider from '../../components/BannersSlider.jsx';
 import Hero from '../../components/Hero.jsx';
 
 function MainPage() {
+  const bannerConfig = [
+    { id: 'top10', title: 'TOP 10 시리즈', fetchUrl: requests.top10Series },
+    { id: 'trending', title: '지금 뜨는 콘텐츠', fetchUrl: requests.trendingAll },
+    { id: 'netflix', title: '넷플릭스 오리지널', fetchUrl: requests.netflixOriginals },
+    { id: 'koreanDrama', title: '한국 드라마', fetchUrl: requests.koreanDramaSeries },
+    { id: 'koreanMovie', title: '한국 영화', fetchUrl: requests.koreanMovies },
+    { id: 'animation', title: '애니메이션', fetchUrl: requests.animationSeries },
+    { id: 'reality', title: '리얼리티 TV', fetchUrl: requests.realityTV },
+    { id: 'sciFiSeries', title: 'SF & 판타지', fetchUrl: requests.sciFiSeries },
+    { id: 'drama', title: '드라마', fetchUrl: requests.dramaSeries },
+    { id: 'crime', title: '범죄 영화', fetchUrl: requests.crimeMovies },
+    { id: 'family', title: '가족 영화', fetchUrl: requests.familyContent }
+  ];
+
   return (
     <div>
       <Hero />
-      <BannersSlider title="TOP 10 시리즈" fetchUrl={requests.top10Series} />
-      <BannersSlider title="지금 뜨는 콘텐츠" fetchUrl={requests.trendingAll} />
-      <BannersSlider title="넷플릭스 오리지널" fetchUrl={requests.netflixOriginals} />
-      <BannersSlider title="한국 드라마" fetchUrl={requests.koreanDramaSeries} />
-      <BannersSlider title="한국 영화" fetchUrl={requests.koreanMovies} />
-      <BannersSlider title="애니메이션" fetchUrl={requests.animationSeries} />
-      <BannersSlider title="리얼리티 TV" fetchUrl={requests.realityTV} />
-      <BannersSlider title="SF & 판타지" fetchUrl={requests.sciFiSeries} />
-      <BannersSlider title="드라마" fetchUrl={requests.dramaSeries} />
-      <BannersSlider title="범죄 영화" fetchUrl={requests.crimeMovies} />
-      <BannersSlider title="가족 영화" fetchUrl={requests.familyContent} />
+      {bannerConfig.map((config) => (
+        <BannersSlider key={config.id} title={config.title} fetchUrl={config.fetchUrl} />
+      ))}
     </div>
   );
 }
