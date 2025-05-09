bce4aea84e4b0fa7f168d4ec7df32e541d1ca5d9
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-09 20:24:12 +0900

    fix: Mainpage 프리뷰 모델 적용

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -20,20 +20,46 @@ function MainPage() {
         <PreviewModal selectedMovie={selectedMovie} setIsModalOpen={setIsModalOpen} />
       )}
 
-      <CardsGridView fetchUrl={requests.trending} clickHandle={clickHandle} />
       <Hero />
-      <BannersSlider title="TOP 10 시리즈" fetchUrl={requests.top10Series} />
-      <BannersSlider title="지금 뜨는 콘텐츠" fetchUrl={requests.trendingAll} />
-      <BannersSlider title="넷플릭스 오리지널" fetchUrl={requests.netflixOriginals} />
-      <BannersSlider title="한국 드라마" fetchUrl={requests.koreanDramaSeries} />
-      <BannersSlider title="한국 영화" fetchUrl={requests.koreanMovies} />
-      <BannersSlider title="애니메이션" fetchUrl={requests.animationSeries} />
-      <BannersSlider title="리얼리티 TV" fetchUrl={requests.realityTV} />
-      <BannersSlider title="SF & 판타지" fetchUrl={requests.sciFiSeries} />
-      <BannersSlider title="드라마" fetchUrl={requests.dramaSeries} />
-      <BannersSlider title="범죄 영화" fetchUrl={requests.crimeMovies} />
-      <BannersSlider title="가족 영화" fetchUrl={requests.familyContent} />
-
+      <BannersSlider
+        title="TOP 10 시리즈"
+        fetchUrl={requests.top10Series}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider
+        title="지금 뜨는 콘텐츠"
+        fetchUrl={requests.trendingAll}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider
+        title="넷플릭스 오리지널"
+        fetchUrl={requests.netflixOriginals}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider
+        title="한국 드라마"
+        fetchUrl={requests.koreanDramaSeries}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider title="한국 영화" fetchUrl={requests.koreanMovies} clickHandle={clickHandle} />
+      <BannersSlider
+        title="애니메이션"
+        fetchUrl={requests.animationSeries}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider title="리얼리티 TV" fetchUrl={requests.realityTV} clickHandle={clickHandle} />
+      <BannersSlider
+        title="SF & 판타지"
+        fetchUrl={requests.sciFiSeries}
+        clickHandle={clickHandle}
+      />
+      <BannersSlider title="드라마" fetchUrl={requests.dramaSeries} clickHandle={clickHandle} />
+      <BannersSlider title="범죄 영화" fetchUrl={requests.crimeMovies} clickHandle={clickHandle} />
+      <BannersSlider
+        title="가족 영화"
+        fetchUrl={requests.familyContent}
+        clickHandle={clickHandle}
+      />
     </div>
   );
 }
