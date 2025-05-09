db12f933ae7cf289f1e082624bebace98edc8644
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-05-09 21:04:31 +0900

    refactor: 배너 아이템 렌더링 코드 분리

--- src/components/BannersSlider.jsx
+++ src/components/BannersSlider.jsx
@@ -31,6 +31,23 @@ function BannersSlider({ title, fetchUrl, clickHandle }) {
     slider.scrollLeft = newScrollLeft;
   };
 
+  const renderBannerItem = (content, index) => (
+    <button
+      type="button"
+      key={content.id}
+      className={`banner-item banner-${index}`}
+      onClick={() => clickHandle(content)}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/original${content.backdrop_path}`}
+        alt={content.title || content.name}
+      />
+      <div className="banner-info">
+        <h3>{content.title || content.name}</h3>
+      </div>
+    </button>
+  );
+
   return (
     <section className="banners-row">
       <h2 className="banners-row-title">{title}</h2>
@@ -43,22 +60,7 @@ function BannersSlider({ title, fetchUrl, clickHandle }) {
           {'<'}
         </button>
         <div className="banner-slider-content" ref={sliderRef}>
-          {contents.map((content, index) => (
-            <button
-              type="button"
-              key={content.id}
-              className={`banner-item banner-${index}`}
-              onClick={() => clickHandle(content)}
-            >
-              <img
-                src={`https://image.tmdb.org/t/p/original${content.backdrop_path}`}
-                alt={content.title || content.name}
-              />
-              <div className="banner-info">
-                <h3>{content.title || content.name}</h3>
-              </div>
-            </button>
-          ))}
+          {contents.map(renderBannerItem)}
         </div>
         <button
           className="banner-slider-arrow arrow-right"
